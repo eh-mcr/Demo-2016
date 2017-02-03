88b5a22fd45a4dfc26795cccdf54a10b6cbb6945
Ralph Stevenson-Jones <ralph@stevenson-jones.co.uk>
2017-02-03 11:45:30 +0000

    updated for linux

--- End Device - Debug/Components/mrfi/subdir_vars.mk
+++ End Device - Debug/Components/mrfi/subdir_vars.mk
@@ -6,17 +6,17 @@
 C_SRCS += \
 ../Components/mrfi/mrfi.c 
 
+C_DEPS += \
+./Components/mrfi/mrfi.d 
+
 OBJS += \
 ./Components/mrfi/mrfi.obj 
 
-C_DEPS += \
-./Components/mrfi/mrfi.pp 
+OBJS__QUOTED += \
+"Components/mrfi/mrfi.obj" 
 
 C_DEPS__QUOTED += \
-"Components\mrfi\mrfi.pp" 
-
-OBJS__QUOTED += \
-"Components\mrfi\mrfi.obj" 
+"Components/mrfi/mrfi.d" 
 
 C_SRCS__QUOTED += \
 "../Components/mrfi/mrfi.c" 
